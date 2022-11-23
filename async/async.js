a07f50f4d74f7fffc4917256da371c8641a98265
Ziv <ziv.perry@zoominfo.com>
2022-11-23 09:03:19 +0200

    examples

--- async/async.js
+++ async/async.js
@@ -1,19 +1,8 @@
-
-
-
-
 document.querySelector('button').addEventListener('click', () => {
     // console.log('0 Before request');
-
-    fetch('/async/data.json').then(response => {
-            response.json().then(data => {
-                console.log(data);
-            })
-        })
-
-        // .then(res => res.json())
-        // .then(res => console.log('>>>', res.json()));
-
+    fetch('/async/data.json')
+        .then(res => res.json())
+        .then(json => console.log('>>>', json));
     //
     // const res = fetch('/async/data.json');
     // console.log('1 After request');
