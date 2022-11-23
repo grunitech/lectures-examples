96f976dc0595893003c38dd1cc5bf0d44115db4d
Ziv <ziv.perry@zoominfo.com>
2022-11-23 12:21:39 +0200

    async

--- async/async.ts
+++ async/async.ts
@@ -1,3 +1,4 @@
+// tsc --target es2015 --watch async/async.ts
 const PAGE_URL = 'https://en.wikipedia.org/w/api.php?action=parse&formatversion=2&prop=text&format=json&origin=*&page=';
 const RAND_URL = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json&origin=*';
 
@@ -8,13 +9,33 @@ async function f1() {
 const f2 = async () => {
 };
 
+
+
+
 // this function will update header & article
 function update(header: string, article: string) {
-    document.querySelector('h1').innerHTML = header;
+    (document.querySelector('h1') as Element).innerHTML = header;
     document.querySelector('article').innerHTML = article;
 }
 
 const getJson = (url: string) => fetch(url).then(response => response.json());
+
+function GetJsonExplained(url: string) {
+    // make HTTP GET request to the URL and return promise of Response
+    return fetch(url).then(function (response: Response) {
+        // call the ".json()" method of "response"
+        return response.json(); // return a Promise of "any"
+    });
+
+
+    // the ".json()" method is a shortcut to ".text()" method & "JSON.parse()"
+    // fetch(url).then(function (response: Response) {
+    //     return response.text().then(function (text: string) {
+    //         return JSON.parse(text);
+    //     });
+    // })
+}
+
 
 async function getRandomArticle() {
     const titleData = await getJson(RAND_URL);
@@ -38,3 +59,78 @@ document.querySelector('button').addEventListener('click', () => {
         update('', '');
     }
 });
+
+
+
+// Liel - great q about multi-return value
+function acceptString(s: string) {
+    console.log(s);
+}
+
+// A
+function giveMeNumber(): number {
+    return 1;
+}
+
+acceptString(giveMeNumber() as unknown as string);
+acceptString(giveMeNumber() as any);
+
+
+// B
+function giveMeNumberOrString(): number | string {
+    return 1;
+}
+
+acceptString(giveMeNumberOrString() as string);
+
+// cast number to string
+acceptString(String(giveMeNumber()));
+acceptString(giveMeNumber().toString()); // <-- preferred
+
+
+// Matan - return undefined on not exists operator (.?)
+
+const matan: any = {
+    foo: {
+        bar: {
+            gaz: [
+                {
+                    a: 1,
+                    b: 2,
+                },
+                {
+                    a: 3,
+                    b: 4
+                }
+            ]
+        }
+    }
+};
+
+// I want to check that gaz exists
+if (matan.foo ) {
+    if (matan.foo.bar && matan.foo.bar.gaz) {
+        // Aurika!!!
+    }
+
+    // error!!!!
+    // matan.foo.bar.gas;
+}
+
+const ex01 = matan.foo;         // V
+const ex02 = matan.foo.bar;     // V
+// const ex03 = matan.foo.goo;
+const ex04 = matan.foo?.goo;     // V
+const ex05 = matan.foo?.goo.rat; // V
+const ex06 = matan.foo.bar.gaz?.boo; // V
+const ex07 = matan?.goo.bar.gaz?.boo; // V
+
+
+if (matan?.foo?.bar?.gaz) {
+    // Aurika!!!
+}
+const gaz = matan?.foo?.bar?.gaz || [];
+// const gaz = matan?.foo?.bar?.gaz ?? [];
+// const gaz = matan?.foo?.bar?.gaz ? matan.foo.bar.gaz : [];
+
+// Matan - KUDOS!
