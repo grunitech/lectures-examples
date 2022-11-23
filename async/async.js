3859799fb70c6402668654f8aa5f708fbfaaf279
Ziv <ziv.perry@zoominfo.com>
2022-11-23 11:47:38 +0200

    async

--- async/async.js
+++ async/async.js
@@ -1,9 +1,3 @@
-//
-// function slowCalc(a: number, b: number, cb: (res: number) => void) {
-//  // takes a lot of time to calculate:
-//  const res = a + b;
-//  cb(res);
-// }
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -13,110 +7,37 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-// this is example to promiose that half of the time reject and the other resolve
-// const promise = new Promise(function (resolve, reject) {
-//     if (Math.random() > .5) {
-//         resolve(1);
-//     } else {
-//         reject(new Error('something went wrong'));
-//     }
-//     // (Math.random() > .5) ? resolve(1) : reject(new Error('something went wrong'));
-// });
-//
-//
-// console.log('start');
-//
-// function allGood(value) {
-//     console.log('resolved value');
-//     console.log(value);
-// }
-//
-// function ohhSnap(error) {
-//     console.log('caught error');
-//     console.log(error);
-// }
-//
-// promise.then(allGood).catch(ohhSnap);
-//
-//
-// console.log('end');
-function getResponse() {
-    return fetch('https://blah-blah.com');
-}
-function getSomething() {
+const PAGE_URL = 'https://en.wikipedia.org/w/api.php?action=parse&formatversion=2&prop=text&format=json&origin=*&page=';
+const RAND_URL = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json&origin=*';
+function f1() {
     return __awaiter(this, void 0, void 0, function* () {
-        return {};
     });
 }
-// A & B ARE IDENTICAL!!!
-// A
-function getFutureValue() {
-    return new Promise(resolve => {
-        resolve(10);
-    });
+const f2 = () => __awaiter(this, void 0, void 0, function* () {
+});
+// this function will update header & article
+function update(header, article) {
+    document.querySelector('h1').innerHTML = header;
+    document.querySelector('article').innerHTML = article;
 }
-// B
-function getAnotherFutureValue() {
+const getJson = (url) => fetch(url).then(response => response.json());
+function getRandomArticle() {
     return __awaiter(this, void 0, void 0, function* () {
-        return 10;
+        const titleData = yield getJson(RAND_URL);
+        const title = titleData.query.random[0].title;
+        const bodyData = yield getJson(PAGE_URL + title);
+        const article = bodyData.parse.text;
+        update(title, article);
     });
 }
-// A & B (then vs await) A & B IDENTICAL!
-// A
-function getFutureSomething() {
-    return new Promise(resolve => {
-        fetch('https://blah-blah.com')
-            .then(response => response.json())
-            .then(data => {
-            resolve(data);
-        });
-    });
-}
-// B
-function getAnotherFutureSomething() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const response = yield fetch('https://blah-blah.com');
-        const data = yield response.json();
-        return data;
-    });
-}
-const abtn = document.querySelector('button');
-const ARANDOM_URL = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json&origin=*';
-function aonButtonClick(event) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const response = yield fetch(RANDOM_URL);
-            const data = yield response.json();
-            document.getElementById('output').innerHTML = data.query.random[0].title;
-        }
-        catch (eeee) {
-            console.log('So baasa, nothing happen', eeee);
-        }
-    });
-}
-btn.addEventListener('click', aonButtonClick);
-//
-//
-// //
-// document.querySelector('button').addEventListener('click', () => {
-//     // console.log('0 Before request');
-//
-//     fetch('/async/data.json').then(response => {
-//             response.json().then(data => {
-//                 console.log(data);
-//             })
-//         })
-//
-//         // .then(res => res.json())
-//         // .then(res => console.log('>>>', res.json()));
-//
-//     //
-//     // const res = fetch('/async/data.json');
-//     // console.log('1 After request');
-//     // res.then(function (data) {
-//     //     return data.json();
-//     // }).then(function (data) {
-//     //     console.log(data);
-//     // })
-//     // console.log('2 After handling');
-// });
+let timer;
+document.querySelector('button').addEventListener('click', () => {
+    if (!timer) {
+        timer = setInterval(getRandomArticle, 5000);
+    }
+    else {
+        clearInterval(timer);
+        timer = undefined;
+        update('', '');
+    }
+});
--- async/async.ts
+++ async/async.ts
@@ -1,4 +1,4 @@
-const PAGE_URL = 'https://en.wikipedia.org/w/api.php?action=parse&formatversion=2&page=Pet_door&prop=text&format=json';
+const PAGE_URL = 'https://en.wikipedia.org/w/api.php?action=parse&formatversion=2&prop=text&format=json&origin=*&page=';
 const RAND_URL = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&format=json&origin=*';
 
 async function f1() {
@@ -8,7 +8,33 @@ async function f1() {
 const f2 = async () => {
 };
 
+// this function will update header & article
+function update(header: string, article: string) {
+    document.querySelector('h1').innerHTML = header;
+    document.querySelector('article').innerHTML = article;
+}
 
-document.querySelector('button').addEventListener('click', async () => {
+const getJson = (url: string) => fetch(url).then(response => response.json());
 
+async function getRandomArticle() {
+    const titleData = await getJson(RAND_URL);
+    const title = titleData.query.random[0].title;
+
+
+    const bodyData = await getJson(PAGE_URL + title);
+    const article = bodyData.parse.text;
+
+    update(title, article);
+}
+
+let timer;
+
+document.querySelector('button').addEventListener('click', () => {
+    if (!timer) {
+        timer = setInterval(getRandomArticle, 5000);
+    } else {
+        clearInterval(timer);
+        timer = undefined;
+        update('', '');
+    }
 });
